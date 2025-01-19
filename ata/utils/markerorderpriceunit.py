1124d123232d4d8f2877149c0c20fda78ea85f60
tyt0815 <yuy0815@icloud.com>
2025-01-19 12:01:37 +0900

    사소한 수정

--- ata/utils/markerorderpriceunit.py
+++ ata/utils/markerorderpriceunit.py
@@ -1,17 +1,37 @@
 
 def upbit_price_unit(item, price):
-    excepted_items = ['ADA', 'ALGO', 'BLUR', 'CELO', 'ELF', 'EOS', 'GRS', 'GRT', 'ICX', 'MANA', 'MINA', 'POL', 'SAND', 'SEI', 'STG', 'TRX']
-    if item in excepted_items:
+    if item in [
+        'ADA', 'ALGO', 'BLUR', 'CELO', 'ELF', 'EOS', 'GRS', 'GRT',
+        'ICX', 'MANA', 'MINA', 'POL', 'SAND', 'SEI', 'STG', 'TRX'
+        ]:
         return 1
-    d = 1
-    while 0.0001 * d <= 100000:
-        if price < 0.0001 * d:
-            return 0.00000001 * d
-        d *= 10
-    if price < 500000:
+    '''
+    https://docs.upbit.com/docs/krw-market-info
+    '''
+    if 0.0001 > price:
+        return 0.00000001
+    elif 0.001 > price:
+        return 0.0000001
+    elif 0.01 > price:
+        return 0.000001
+    elif 0.1 > price:
+        return 0.00001
+    elif 1 > price:
+        return 0.0001
+    elif 10 > price:
+        return 0.001
+    elif 100 > price:
+        return 0.01
+    elif 1000 > price:
+        return 0.1
+    elif 10000 > price:
+        return 1
+    elif 100000 > price:
+        return 10
+    elif 500000 > price:
         return 50
-    elif price < 1000000:
+    elif 1000000 > price:
         return 100
-    elif price < 2000000:
+    elif 2000000 > price:
         return 500
     return 1000
